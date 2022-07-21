64ec1b25381d131ebda72ffdf9e09d54f587d2e8
tommivk <tommikokk@gmail.com>
2022-07-21 20:44:40 +0300

    add login and logout

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from flask import Flask
-from flask import render_template, request
-import flask
+from flask import render_template, request, session, redirect
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import check_password_hash, generate_password_hash
 from os import getenv
 
 app = Flask(__name__)
+app.secret_key = getenv("SECRET_KEY")
 
 DATABASE_URI = getenv('DATABASE_URI')
 app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URI
@@ -14,7 +14,8 @@ db = SQLAlchemy(app)
 
 @app.route("/")
 def index():
-    return render_template("index.html") 
+    return render_template("index.html")
+
 
 @app.route("/signup", methods=["POST", "GET"])
 def signup():
@@ -23,9 +24,41 @@ def signup():
         password = request.form["password"]
         hash = generate_password_hash(password)
         sql = "INSERT INTO Users (username, password_hash) VALUES(:username, :password_hash)"
-        db.session.execute(sql, {"username":username, "password_hash":hash})
+        db.session.execute(sql, {"username": username, "password_hash": hash})
         db.session.commit()
 
-        return render_template("index.html") 
-    else: 
-        return render_template("signup.html") 
+        return render_template("index.html")
+    else:
+        return render_template("signup.html")
+
+
+@app.route("/login", methods=["GET"])
+def login_form():
+    return render_template("login_form.html")
+
+
+@app.route("/login", methods=["POST"])
+def login_post():
+    username = request.form["username"]
+    password = request.form["password"]
+
+    sql = "SELECT id, password_hash FROM users WHERE username=:username"
+    result = db.session.execute(sql, {"username": username})
+    user = result.fetchone()
+
+    if not user:
+        return "Invalid username"
+    else:
+        hash = user.password_hash
+
+        if check_password_hash(hash, password):
+            session["username"] = username
+            return redirect("/")
+        else:
+            return "Invalid password"
+
+
+@app.route("/logout")
+def logout():
+    del session["username"]
+    return redirect("/")
