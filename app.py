590ce3359c97ec7ca3dc9854c76d265aabad99e2
tommivk <tommikokk@gmail.com>
2022-08-04 16:49:50 +0300

    redirect to recipes after login

--- app.py
+++ app.py
@@ -82,7 +82,7 @@ def login_post():
             if user.admin == True:
                 session["admin"] = True
             flash(f"Logged in as {user.username}")
-            return redirect("/")
+            return redirect("/drinks")
         else:
             flash("Invalid credentials", "error")
             return redirect("/login")
